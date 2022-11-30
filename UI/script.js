6bdfa7c24b3fee70e8ddfa05473b34c3499f3391
Deepanshu9585 <107471930+Deepanshu9585@users.noreply.github.com>
2022-11-30 16:28:01 +0530

    Add files via upload

---
+++ UI/script.js
@@ -0,0 +1,13 @@
+var input = document.getElementById('inputDefault');
+var input = document.getElementById('bttn');
+
+function addTask() {
+    var taskValue = input.value;
+
+}
+if (taskValue = '')
+{
+    alertText.innerText = 'Please enter a task';
+}
+
+button.onclick = add.Task;
